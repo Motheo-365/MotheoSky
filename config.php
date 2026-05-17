8f31cf86e00008348025fa451047d02c865ce916
Venderludra <129943009+Venderludra@users.noreply.github.com>
2026-05-17 20:30:17 +0200

    Update database connection details in config.php

--- config.php
+++ config.php
@@ -1,17 +1,16 @@
 <?php
-    //Local Database Connection
-    $host = "127.0.0.1";
-    $db = "cos216_asynconus";
-    $user = "root";
-    $dbpass = "";
-    $port = 3306;
+    $host = "wheatley.cs.up.ac.za";
+    $username = "u24667642";
+    $password = "5CFGFYGBZMXOKFK5AFD4NDT5JLLSYIW4";
+    $db = "u24667642_HA";
 
-    $conn = new mysqli($host, $user, $dbpass, $db, $port);
+    $conn = new mysqli($host,$username,$password,$db);
 
-    if ($conn->connect_error) {
+    if($conn->connect_error){
         die(json_encode([
             "status" => "error",
             "message" => "Database connection failed"
-        ]));
+    ]));
     }
+    
 ?>
